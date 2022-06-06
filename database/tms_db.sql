a7fdbf59f03330a750c26f115330e7b27787f29e
RimOuarrak <82288348+RimOuarrak@users.noreply.github.com>
2022-06-06 14:40:26 +0000

    db

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 03, 2020 at 08:37 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 06 juin 2022 à 16:39
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,67 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tms_db`
+-- Base de données : `tms_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project_list`
+-- Structure de la table `docs`
+--
+
+CREATE TABLE `docs` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `docs`
+--
+
+INSERT INTO `docs` (`id`, `project_id`, `nom`, `link`) VALUES
+(3, 13, 'test link', 'https://github.com/RimOuarrak/Menara-service');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL,
+  `downloads` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `org`
+--
+
+CREATE TABLE `org` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `num` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `org`
+--
+
+INSERT INTO `org` (`id`, `name`, `email`, `num`) VALUES
+(12, 'COUR D&amp;#x2019;APPEL', 'crdpl@gmail.com', '+2126969696'),
+(13, 'test', 'll@kk.com', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `project_list`
 --
 
 CREATE TABLE `project_list` (
@@ -36,21 +90,30 @@ CREATE TABLE `project_list` (
   `end_date` date NOT NULL,
   `manager_id` int(30) NOT NULL,
   `user_ids` text NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `num_ordr` int(11) NOT NULL,
+  `num_offr` varchar(20) NOT NULL,
+  `ctn` float NOT NULL,
+  `est` float NOT NULL,
+  `est_min` float NOT NULL,
+  `est_max` float NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `hr` varchar(255) NOT NULL,
+  `qc` varchar(255) NOT NULL,
+  `org_id` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `project_list`
+-- Déchargement des données de la table `project_list`
 --
 
-INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
-(1, 'Sample Project', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;							', 0, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56'),
-(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54');
+INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`, `num_ordr`, `num_offr`, `ctn`, `est`, `est_min`, `est_max`, `ville`, `hr`, `qc`, `org_id`) VALUES
+(13, '', '&lt;p&gt;						description&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;', 0, '2002-03-13', '2039-03-13', 2, '', '2022-06-02 15:32:34', 3, '40', 88, 69, 77, 99, 'merrakch', '2', 'sas/s', '12');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_settings`
+-- Structure de la table `system_settings`
 --
 
 CREATE TABLE `system_settings` (
@@ -63,16 +126,16 @@ CREATE TABLE `system_settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `system_settings`
+-- Déchargement des données de la table `system_settings`
 --
 
 INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
-(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '');
+(1, 'Menara services', '', '+212', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task_list`
+-- Structure de la table `task_list`
 --
 
 CREATE TABLE `task_list` (
@@ -81,23 +144,22 @@ CREATE TABLE `task_list` (
   `task` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `status` tinyint(4) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `bq_name` varchar(30) NOT NULL,
+  `agence` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `task_list`
+-- Déchargement des données de la table `task_list`
 --
 
-INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`) VALUES
-(1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
-(2, 1, 'Sample Task 2', 'Sample Task 2							', 1, '2020-12-03 13:50:15'),
-(3, 2, 'Task Test', 'Sample', 1, '2020-12-03 13:52:25'),
-(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40');
+INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`, `bq_name`, `agence`) VALUES
+(17, 13, 'Caution provisoire', '														', 1, '2022-06-02 19:52:32', 'Attijari Wafabank', 'OMAR RIFFI CASABLANCA ');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
@@ -112,12 +174,12 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
 (1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
-(2, 'John', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 2, '1606978560_avatar.jpg', '2020-12-03 09:26:03'),
+(2, 'Ibtissam ', 'Salhi', 'isalhi@gmail.com', 'd532c37bae98c2a336a946f30668f812', 2, '1652358780_logo.png', '2020-12-03 09:26:03'),
 (3, 'Claire', 'Blake', 'cblake@sample.com', '4744ddea876b11dcb1d169fadf494418', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
 (4, 'George', 'Wilson', 'gwilson@sample.com', 'd40242fb23c45206fadee4e2418f274f', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
 (5, 'Mike', 'Williams', 'mwilliams@sample.com', '3cc93e9a6741d8b40460457139cf8ced', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06');
@@ -125,7 +187,7 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`,
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_productivity`
+-- Structure de la table `user_productivity`
 --
 
 CREATE TABLE `user_productivity` (
@@ -143,7 +205,7 @@ CREATE TABLE `user_productivity` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_productivity`
+-- Déchargement des données de la table `user_productivity`
 --
 
 INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
@@ -153,72 +215,118 @@ INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subj
 (4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `project_list`
+-- Index pour la table `docs`
+--
+ALTER TABLE `docs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `org`
+--
+ALTER TABLE `org`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `project_list`
 --
 ALTER TABLE `project_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `system_settings`
+-- Index pour la table `system_settings`
 --
 ALTER TABLE `system_settings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `task_list`
+-- Index pour la table `task_list`
 --
 ALTER TABLE `task_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_productivity`
+-- Index pour la table `user_productivity`
 --
 ALTER TABLE `user_productivity`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `project_list`
+-- AUTO_INCREMENT pour la table `docs`
+--
+ALTER TABLE `docs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `org`
+--
+ALTER TABLE `org`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `system_settings`
+-- AUTO_INCREMENT pour la table `system_settings`
 --
 ALTER TABLE `system_settings`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `task_list`
+-- AUTO_INCREMENT pour la table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `user_productivity`
+-- AUTO_INCREMENT pour la table `user_productivity`
 --
 ALTER TABLE `user_productivity`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `docs`
+--
+ALTER TABLE `docs`
+  ADD CONSTRAINT `docs_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project_list` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
